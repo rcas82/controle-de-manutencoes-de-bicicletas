74b7056e470612282b37e7f3b3a089ef6fd2e149
Rafael Cassone <rafaelcassone@gmail.com>
2022-07-29 08:58:29 -0300

    Atividade 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponent } from './primeiro/primeiro.component';
+import { SegundoComponent } from './segundo/segundo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponent,
+    SegundoComponent
   ],
   imports: [
     BrowserModule,
